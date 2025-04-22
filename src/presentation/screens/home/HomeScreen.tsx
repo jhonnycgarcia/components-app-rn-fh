2809673e76a1bde3fe55828f10588ec0213d3ada
Jhonny <jhonnycgarcia@gmail.com>
2025-04-22 09:53:51 -0500

    menu principal y estilos

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,11 +1,85 @@
 import { View, Text } from 'react-native';
-import { AirplaneIcon } from '../../icons/Icons';
+import { globalStyles } from '../../../config/theme/theme';
+import { ScrollView } from 'react-native-gesture-handler';
+import { Title } from '../../components';
+
+export const menuItems = [
+  // 01-animationMenuItems
+  {
+    name: 'Animation 101',
+    icon: 'cube-outline',
+    component: 'Animation101Screen',
+  },
+  {
+    name: 'Animation 102',
+    icon: 'albums-outline',
+    component: 'Animation102Screen',
+  },
+  // 02-menuItems
+  {
+    name: 'Pull to refresh',
+    icon: 'refresh-outline',
+    component: 'PullToRefreshScreen',
+  },
+  {
+    name: 'Section List',
+    icon: 'list-outline',
+    component: 'CustomSectionListScreen',
+  },
+  {
+    name: 'Modal',
+    icon: 'copy-outline',
+    component: 'ModalScreen',
+  },
+  {
+    name: 'InfiniteScroll',
+    icon: 'download-outline',
+    component: 'InfiniteScrollScreen',
+  },
+  {
+    name: 'Slides',
+    icon: 'flower-outline',
+    component: 'SlidesScreen',
+  },
+  {
+    name: 'Themes',
+    icon: 'flask-outline',
+    component: 'ChangeThemeScreen',
+  },
+
+  // 03- uiMenuItems
+  {
+    name: 'Switches',
+    icon: 'toggle-outline',
+    component: 'SwitchScreen',
+  },
+  {
+    name: 'Alerts',
+    icon: 'alert-circle-outline',
+    component: 'AlertScreen',
+  },
+  {
+    name: 'TextInputs',
+    icon: 'document-text-outline',
+    component: 'TextInputScreen',
+  },
+];
 
 export const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-      <AirplaneIcon />
+    <View style={globalStyles.mainContainer}>
+      <View style={globalStyles.globalMargin}>
+        <ScrollView>
+          <Title text="HomeScreen" safe />
+          {
+            menuItems.map(({ name, component }) => (
+              <Text key={component}>
+                <Text>{name}</Text>
+              </Text>
+            ))
+          }
+        </ScrollView>
+      </View>
     </View>
   );
 };
