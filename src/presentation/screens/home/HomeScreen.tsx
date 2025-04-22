4b1fec230f94cdfd5ed484132a497e23970ca35b
Jhonny <jhonnycgarcia@gmail.com>
2025-04-22 09:34:51 -0500

    instalacion de iconos de ionicons

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,9 +1,11 @@
 import { View, Text } from 'react-native';
+import { AirplaneIcon } from '../../icons/Icons';
 
 export const HomeScreen = () => {
   return (
     <View>
       <Text>HomeScreen</Text>
+      <AirplaneIcon />
     </View>
   );
 };
