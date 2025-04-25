9f892d0c8386838a203a98bf00bbfe39fb69eff2
Jhonny <jhonnycgarcia@gmail.com>
2025-04-24 23:10:09 -0500

    scroll y teclado

--- src/presentation/screens/inputs/TextInputScreen.tsx
+++ src/presentation/screens/inputs/TextInputScreen.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { KeyboardAvoidingView, Platform, ScrollView, Text, TextInput, View } from 'react-native';
 
 import { Card, CustomView, Title } from '../../components';
 import { globalStyles } from '../../../config/theme/theme';
@@ -11,51 +11,69 @@ export const TextInputScreen = () => {
         phone: '',
     });
     return (
-        <CustomView margin>
-            <Title text="TextInputScreen" safe />
-            <Card>
-                <TextInput
-                    style={globalStyles.input}
-                    placeholder="Ingrese su nombre completo"
-                    autoCapitalize={'words'}
-                    autoCorrect={false}
-                    onChangeText={(value) => console.log(value)}
-                />
-                <TextInput
-                    style={globalStyles.input}
-                    placeholder="Ingrese su email"
-                    autoCapitalize={'none'}
-                    autoCorrect={false}
-                    keyboardType="email-address"
-                    onChangeText={(value) => setForm({ ...form, email: value })}
-                />
-                <TextInput
-                    style={globalStyles.input}
-                    placeholder="Ingrese su teléfono"
-                    autoCapitalize={'none'}
-                    autoCorrect={false}
-                    keyboardType="phone-pad"
-                    onChangeText={(value) => setForm({ ...form, phone: value })}
-                />
-            </Card>
-            <View style={{ height: 10 }} />
-            <Card>
-                <Text>{JSON.stringify(form, null, 2)}</Text>
-                <Text>{JSON.stringify(form, null, 2)}</Text>
-                <Text>{JSON.stringify(form, null, 2)}</Text>
-                <Text>{JSON.stringify(form, null, 2)}</Text>
-                <Text>{JSON.stringify(form, null, 2)}</Text>
-                <Text>{JSON.stringify(form, null, 2)}</Text>
-                <Text>{JSON.stringify(form, null, 2)}</Text>
-                <Text>{JSON.stringify(form, null, 2)}</Text>
-                <Text>{JSON.stringify(form, null, 2)}</Text>
-                <Text>{JSON.stringify(form, null, 2)}</Text>
-                <Text>{JSON.stringify(form, null, 2)}</Text>
-                <Text>{JSON.stringify(form, null, 2)}</Text>
-                <Text>{JSON.stringify(form, null, 2)}</Text>
-                <Text>{JSON.stringify(form, null, 2)}</Text>
-                <Text>{JSON.stringify(form, null, 2)}</Text>
-            </Card>
-        </CustomView>
+        <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        >
+            <ScrollView>
+                <CustomView margin>
+                    <Title text="TextInputScreen" safe />
+                    <Card>
+                        <TextInput
+                            style={globalStyles.input}
+                            placeholder="Ingrese su nombre completo"
+                            autoCapitalize={'words'}
+                            autoCorrect={false}
+                            onChangeText={(value) => console.log(value)}
+                        />
+                        <TextInput
+                            style={globalStyles.input}
+                            placeholder="Ingrese su email"
+                            autoCapitalize={'none'}
+                            autoCorrect={false}
+                            keyboardType="email-address"
+                            onChangeText={(value) => setForm({ ...form, email: value })}
+                        />
+                        <TextInput
+                            style={globalStyles.input}
+                            placeholder="Ingrese su teléfono"
+                            autoCapitalize={'none'}
+                            autoCorrect={false}
+                            keyboardType="phone-pad"
+                            onChangeText={(value) => setForm({ ...form, phone: value })}
+                        />
+                    </Card>
+                    <View style={{ height: 10 }} />
+                    <Card>
+                        <Text>{JSON.stringify(form, null, 2)}</Text>
+                        <Text>{JSON.stringify(form, null, 2)}</Text>
+                        <Text>{JSON.stringify(form, null, 2)}</Text>
+                        <Text>{JSON.stringify(form, null, 2)}</Text>
+                        <Text>{JSON.stringify(form, null, 2)}</Text>
+                        <Text>{JSON.stringify(form, null, 2)}</Text>
+                        <Text>{JSON.stringify(form, null, 2)}</Text>
+                        <Text>{JSON.stringify(form, null, 2)}</Text>
+                        <Text>{JSON.stringify(form, null, 2)}</Text>
+                        <Text>{JSON.stringify(form, null, 2)}</Text>
+                        <Text>{JSON.stringify(form, null, 2)}</Text>
+                        <Text>{JSON.stringify(form, null, 2)}</Text>
+                        <Text>{JSON.stringify(form, null, 2)}</Text>
+                        <Text>{JSON.stringify(form, null, 2)}</Text>
+                        <Text>{JSON.stringify(form, null, 2)}</Text>
+                    </Card>
+                    <View style={{ height: 10 }} />
+                    <Card>
+                        <TextInput
+                            style={globalStyles.input}
+                            placeholder="Ingrese su email"
+                            autoCapitalize={'none'}
+                            autoCorrect={false}
+                            keyboardType="email-address"
+                            onChangeText={(value) => setForm({ ...form, email: value })}
+                        />
+                    </Card>
+                </CustomView>
+                <View style={{ height: 50 }} />
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 };
