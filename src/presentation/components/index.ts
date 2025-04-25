a8db87689db782dfe303d919e6c4af06f4d5c02f
Jhonny <jhonnycgarcia@gmail.com>
2025-04-24 20:43:36 -0500

    componente switch

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,5 +1,6 @@
 export * from './ui/Button';
 export * from './ui/Card';
+export * from './ui/CustomSwitch';
 export * from './ui/CustomView';
 export * from './ui/MenuItem';
 export * from './ui/Title';
--- src/presentation/screens/switches/SwitchScreen.tsx
+++ src/presentation/screens/switches/SwitchScreen.tsx
@@ -1,8 +1,36 @@
-import { CustomView } from '../../components';
+import { useState } from 'react';
+import { Card, CustomSwitch, CustomView } from '../../components';
 
 export const SwitchScreen = () => {
+    const [state, setState] = useState({
+        isActive: true,
+        isHungry: false,
+        isHappy: true,
+    });
+
+    const toggleSwitch = (key: keyof typeof state) => {
+        setState({ ...state, [key]: !state[key] });
+    };
+
     return (
         <CustomView style={{ marginTop: 100, paddingHorizontal: 10 }}>
+            <Card>
+                <CustomSwitch
+                    isOn={state.isActive}
+                    text="Activo"
+                    onChange={() => toggleSwitch('isActive')}
+                />
+                <CustomSwitch
+                    isOn={state.isHungry}
+                    text="Hambriento"
+                    onChange={() => toggleSwitch('isHungry')}
+                />
+                <CustomSwitch
+                    isOn={state.isHappy}
+                    text="Feliz"
+                    onChange={() => toggleSwitch('isHappy')}
+                />
+            </Card>
         </CustomView>
     );
 };
