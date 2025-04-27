ee195fd87b5ccdee9b5bbee933eb7221ed8cb984
Jhonny <jhonnycgarcia@gmail.com>
2025-04-27 18:01:10 -0500

    pull to refresh

--- src/presentation/navigation/Navigator.tsx
+++ src/presentation/navigation/Navigator.tsx
@@ -4,6 +4,7 @@ import { Animation101Screen, Animation102Screen } from '../screens/animations';
 import { SwitchScreen } from '../screens/switches';
 import { AlertScreen } from '../screens/alerts/AlertScreen';
 import { TextInputScreen } from '../screens/inputs/TextInputScreen';
+import { PullToRefreshScreen } from '../screens/ui/PullToRefreshScreen';
 
 const Stack = createStackNavigator();
 
@@ -17,6 +18,8 @@ export function Navigator() {
       <Stack.Screen name="SwitchScreen" component={SwitchScreen} />
       <Stack.Screen name="AlertScreen" component={AlertScreen} />
       <Stack.Screen name="TextInputScreen" component={TextInputScreen} />
+
+      <Stack.Screen name="PullToRefreshScreen" component={PullToRefreshScreen} />
     </Stack.Navigator>
   );
 }
